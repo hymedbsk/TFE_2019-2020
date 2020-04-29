56e07d46bc8e0cfe63bf3e6239fbaf632505dad2
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-04-29 18:48:05 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,45 @@
 |
 */
 
-Route::get('/', 'ViewController@getview');
-Route::get('/welcome','ViewController@getAcc')->name('welcome');
-Route::post('/welcome', 'ViewController@postForm')->name('welcome.contact');
-Route::view('/plateforme', 'plateforme');
-Route::get('/profil', 'ChangerPasswordController@index');
-Route::post('/profil', 'ChangerPasswordController@store')->name('change.password');
-Auth::routes();
+Route::get('/', function () {
+    return view('accueil');
+});
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/', 'ContactController@postForm')->name('contact.form');
+Route::view('/plateforme', 'plateforme');
+Route::get('/profil', 'ChangerPasswordController@index')->middleware('verified');
+Route::post('/profil', 'ChangerPasswordController@store')->name('change.password')->middleware('verified');
+
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+
+
+Route::resource('post', 'PostController')->middleware('verified');
+Route::resource('user', 'UserController')->middleware('verified');
+
+Auth::routes(['verify'=> true]);
+
 
 Route::get('/download/{file}', function ($file='') {
     return response()->download(storage_path('app/public/'.$file));
+})->middleware('verified');
+
+Route::post('post/search', 'PostController@search')->name('post.search');
+Route::put('user/{id}', 'UserController@admin')->name('user.admin');
+Route::put('user/{user}', 'UserController@delAdmin')->name('user.delAdmin');
+Route::get('verification', 'UserController@getCheck');
+Route::put('verification/{user}', 'UserController@check')->name('user.check');
+
+Route::get('role', 'RoleController@index' )->name('role.index');
+Route::get('role/gestion', 'RoleController@show' )->name('role.show');
+Route::get('role/gestion/create', 'RoleController@create' )->name('role.create');
+Route::post('role/gestion/create', 'RoleController@store' )->name('role.store');
+Route::post('role/{role}/role', 'RoleController@change')->name('role.change');
+Route::delete('role/gestion/{role}', 'RoleController@destroy')->name('role.delete');
+Route::get('test', function () {
+
+$User = App\User::first();
+$Roles = App\Role::first();
+$User->roles()->attach($Roles);
 });
-
-Route::resource('post', 'PostController', ['except' => ['show', 'edit', 'update']]);
-
-
-
