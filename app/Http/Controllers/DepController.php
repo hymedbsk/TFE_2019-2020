1c6b2c406de4526c31afceb7b5eca8fafd8ee61d
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-05-09 18:21:56 +0200

    Add files via upload

--- app/Http/Controllers/DepController.php
+++ app/Http/Controllers/DepController.php
@@ -7,7 +7,8 @@ use App\Budget;
 use App\Gain;
 use App\Depense;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Crypt;
+use Barryvdh\DomPDF\Facade as PDF;
 class DepController extends Controller
 {
     private $totDepense = 0;
@@ -19,21 +20,26 @@ class DepController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index($id){
+    public function index($ids){
+
+        $id = Crypt::decrypt($ids);
 
         $totals = DB::table('budget')->where('id', '=','2')->pluck('total');
         $gains = Gain::all()->where('budg_id','=',$id);
         $depenses =  Depense::all()->where('budg_id','=',$id);
+        $budget = Budget::findOrFail($id);
+
+
 
         foreach ($depenses as $depense) {
 
-            $this->totDepense += $depense->qte;
+            $this->totDepense += $depense->montant;
 
         }
 
         foreach ($gains as $gain) {
 
-            $this->totGain += $gain->qte;
+            $this->totGain += $gain->montant;
 
         }
 
@@ -45,7 +51,7 @@ class DepController extends Controller
 
         $this->tot = $total - $this->totDepense;
         $this->tot +=  $this->totGain;
-            return view('depense.list', compact('depenses','gains','totals'))->with('totGain', $this->totGain)->with('totDepense',$this->totDepense)->with('tot',$this->tot);
+            return view('depense.list', compact('depenses','gains','totals','budget'))->with('totGain', $this->totGain)->with('totDepense',$this->totDepense)->with('tot',$this->tot);
 
     }
 
@@ -54,10 +60,14 @@ class DepController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($ids)
     {
+        $id = Crypt::decrypt($ids);
+        $budget = Budget::findOrFail($id);
 
-        return view('depense.add');
+
+        return view('depense.add', compact('budget'));
+
     }
 
     /**
@@ -66,9 +76,20 @@ class DepController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request,$ids)
     {
-        //
+        $id = Crypt::decrypt($ids);
+        echo $ids;
+
+        $depense = new Depense;
+        $depense->libelle = $request->input('libelle');
+        $depense->montant = $request->input('montant');
+        $depense->User_id = $request->user()->User_id;
+        $depense->budg_id = $id;
+        $depense->description = $request->input('description');
+        $depense->save();
+
+        return redirect('budget/'.Crypt::encrypt($id));
     }
 
     /**
@@ -77,9 +98,39 @@ class DepController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function pdf($ids)
     {
-        //
+        $id = Crypt::decrypt($ids);
+
+        $totals = DB::table('budget')->where('id', '=','2')->pluck('total');
+        $gains = Gain::all()->where('budg_id','=',$id);
+        $depenses =  Depense::all()->where('budg_id','=',$id);
+        $budget = Budget::findOrFail($id);
+
+
+
+        foreach ($depenses as $depense) {
+
+            $this->totDepense += $depense->montant;
+
+        }
+
+        foreach ($gains as $gain) {
+
+            $this->totGain += $gain->montant;
+
+        }
+
+        foreach ($totals as $total) {
+
+            $this->montant = $total;
+
+        }
+
+        $this->tot = $total - $this->totDepense;
+        $this->tot +=  $this->totGain;
+        $pdf = PDF::loadView('pdf', compact('depenses','gains','totals','budget'),['totDepense'=> $this->totDepense, 'totGain'=>$this->totGain, 'tot'=>$this->tot]);
+        return $pdf->download('compte.pdf');
     }
 
     /**
---
+++ app/Http/Controllers/GainController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use App\Budget;
+use App\Gain;
+class GainController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($ids)
+    {
+        $id = Crypt::decrypt($ids);
+        $budget = Budget::findOrFail($id);
+
+
+        return view('gain.add', compact('budget'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, $ids)
+    {
+        $id = Crypt::decrypt($ids);
+        $gain = new Gain;
+
+        $gain->libelle = $request->input('libelle');
+        $gain->montant = $request->input('montant');
+        $gain->User_id = $request->user()->User_id;
+        $gain->budg_id = $id;
+        $gain->description = $request->input('description');
+        $gain->save();
+
+        return redirect('budget/'.Crypt::encrypt($id));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
