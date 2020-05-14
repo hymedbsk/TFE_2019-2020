630448c92037960836b905a1fcbc74a1972f68b2
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2020-05-14 18:00:45 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('accueil');
 });
 
@@ -25,31 +25,69 @@ Route::post('/profil', 'ChangerPasswordController@store')->name('change.password
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
 
-Route::resource('post', 'PostController')->middleware('verified');
-Route::resource('user', 'UserController')->middleware('verified');
+Route::resource('post', 'PostController');
+Route::resource('user', 'UserController');
 
 Auth::routes(['verify'=> true]);
 
 
 Route::get('/download/{file}', function ($file='') {
-    return response()->download(storage_path('app/public/'.$file));
+    return response()->download('app/public/'.$file);
 })->middleware('verified');
 
 Route::post('post/search', 'PostController@search')->name('post.search');
-Route::put('user/{id}', 'UserController@admin')->name('user.admin');
-Route::put('user/{user}', 'UserController@delAdmin')->name('user.delAdmin');
-Route::get('verification', 'UserController@getCheck');
-Route::put('verification/{user}', 'UserController@check')->name('user.check');
+Route::put('user/{id}', 'UserController@admin')->name('user.admin')->middleware('auth','admin','verified','president','vicepresident');
+Route::put('user/{user}', 'UserController@delAdmin')->name('user.delAdmin')->middleware('auth','admin','verified','president','vicepresident');
+Route::get('verification', 'UserController@getCheck')->middleware('auth','admin','verified','president','vicepresident');
+Route::put('verification/{user}', 'UserController@check')->name('user.check')->middleware('auth','admin','verified','president','vicepresident');
 
-Route::get('role', 'RoleController@index' )->name('role.index');
-Route::get('role/gestion', 'RoleController@show' )->name('role.show');
-Route::get('role/gestion/create', 'RoleController@create' )->name('role.create');
-Route::post('role/gestion/create', 'RoleController@store' )->name('role.store');
-Route::post('role/{role}/role', 'RoleController@change')->name('role.change');
-Route::delete('role/gestion/{role}', 'RoleController@destroy')->name('role.delete');
-Route::get('test', function () {
+Route::get('role', 'RoleController@index' )->name('role.index')->middleware('auth','verified');
+Route::get('role/gestion', 'RoleController@show' )->name('role.show')->middleware('auth','verified');
+Route::get('role/gestion/create', 'RoleController@create' )->name('role.create')->middleware('auth','verified');
+Route::post('role/gestion/create', 'RoleController@store' )->name('role.store')->middleware('auth','verified');
+Route::put('role/{role}/add', 'RoleController@change')->name('role.change');
+Route::delete('role/gestion/{role}', 'RoleController@destroy')->name('role.destroy')->middleware('auth','verified');
+Route::delete('role/{role}', 'RoleController@delete')->name('role.delete')->middleware('auth','verified');
+Route::get('/', 'ChatsController@index');
+Route::get('messages', 'ChatsController@fetchMessages');
+Route::post('messages', 'ChatsController@sendMessage');
 
-$User = App\User::first();
-$Roles = App\Role::first();
-$User->roles()->attach($Roles);
-});
+Route::get('test', 'FileController@index');
+Route::get('/ViewerJS/{all?}', array('as' => 'pdfViewer', 'uses' => 'ViewFileController@pdfViewer'));
+
+Route::view('document', 'sharepoint');
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('post/test', 'PostController@show')->name('file.show');
+
+Route::get('document', 'DocController@index')->name('doc.index');
+Route::get('document/add', 'DocController@create')->name('doc.add');
+Route::post('document/add', 'DocController@store')->name('doc.store');
+
+Route::get('document/{document}/list', 'DocController@show')->name('doc.show');
+
+
+Route::get('document/file/test', 'FileController@index')->name('file.index');
+Route::get('document/{document}/file', 'FileController@create')->name('file.add');
+Route::post('document/file/store', 'FileController@store')->name('file.store');
+
+Route::view('test','file.test');
+
+Route::get('budget', 'BudgetController@index')->name('budget.index');
+Route::get('budget/create', 'BudgetController@create')->name('budget.index');
+Route::post('budget/create', 'BudgetController@store')->name('budget.store');
+Route::get('budget/{budget}/edit', 'BudgetController@edit')->name('budget.edit');
+Route::put('budget/{budget}', 'BudgetController@update')->name('budget.update');
+Route::delete('budget/{budget}', 'BudgetController@destroy')->name('budget.destroy');
+
+Route::get('budget/{budget}', 'DepController@index')->name('depense.index');
+Route::get('budget/{budget}/depense','DepController@create')->name('depense.create');
+Route::post('budget/{budget}/depense','DepController@store')->name('depense.store');
+
+Route::get('budget/{budget}/gain','GainController@create')->name('gain.create');
+Route::post('budget/{budget}/gain','GainController@store')->name('gain.store');
+
+Route::get('/downloadPDF/{id}','DepController@pdf');
